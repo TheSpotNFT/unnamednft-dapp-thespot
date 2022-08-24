b0b4903d405eac326fed4cc293bfb674548b7f2c
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2022-08-23 20:40:46 -0700

    new images

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from '../assets/logo.png'
+import logo from '../assets/unnamedimg.png'
 import LogoutButton from './Logout'
 import { GiHamburgerMenu } from "react-icons/gi"
 
