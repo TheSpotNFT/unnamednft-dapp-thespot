9ba7800b2b37f968908182fcc73c24e8ac14dcf7
The Spot <96501055+TheSpotNFT@users.noreply.github.com>
2022-10-30 21:07:28 -0700

    Update Authenticate.jsx

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -9,8 +9,8 @@ export default function Authenticate() {
       <div className="m-auto">
         <img src={unnamednft} className="max-w-md" alt="Goatd Logo"></img>
         <div className="align-middle px-40 py-10">
-          <button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
-    hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l" onClick={() => authenticate({ signingMessage: "Sign in to The Spot!" })}><b>Authenticate</b></button>
+          <a href="https://thespot.art" target="_blank" rel="noreferrer"><button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+    hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l"><b>Authenticate</b></button></a>
         </div>
       </div>
     </div>
